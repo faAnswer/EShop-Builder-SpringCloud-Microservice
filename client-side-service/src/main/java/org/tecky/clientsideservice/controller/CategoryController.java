b02bf4c30a4091a8a14c56e4cb67bd3bd5455255
faAnswer <oscarogc@gmail.com>
2022-12-12 07:38:02 +0800

    20221212

--- client-side-service/src/main/java/org/tecky/clientsideservice/controller/CategoryController.java
+++ client-side-service/src/main/java/org/tecky/clientsideservice/controller/CategoryController.java
@@ -1,34 +1,55 @@
 package org.tecky.clientsideservice.controller;
 
+import org.faAnswer.web.util.CustomException;
+import org.faAnswer.web.util.json.ResponseListObject;
+import org.faAnswer.web.util.json.ResponseObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.tecky.clientsideservice.mapper.TypeListEntityRepository;
+import org.tecky.clientsideservice.service.intf.ICategoryService;
+import org.tecky.common.dto.CategoryDTO;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class CategoryController {
 
     @Autowired
-    TypeListEntityRepository typeListEntityRepository;
+    ICategoryService iCategoryService;
 
     @GetMapping("/v1/category")
-    public String getCategory(@PathParam("clientId") String clienId, HttpServletRequest request, HttpServletResponse response){
+    public ResponseEntity<?> getCategory(@PathParam("clientId") String clienId, HttpServletRequest request, HttpServletResponse response)  {
 
+        List<CategoryDTO> categoryDTOList;
 
+        ResponseEntity<?> res;
 
+        try{
 
+            categoryDTOList = iCategoryService.getCategory(clienId);
 
+        } catch(Exception e){
 
+            throw new CustomException(500, "Error in Service : GET /api/v1/category");
+        }
 
+        try{
 
+            res = ResponseListObject.builder().setObjectPayLoad(categoryDTOList).create(200);
 
+        } catch(Exception e){
 
-        return "category";
+            throw new CustomException(500, "Error in ResponseEntity : GET /api/v1/category");
+        }
+        
+        return res;
     }
 }
