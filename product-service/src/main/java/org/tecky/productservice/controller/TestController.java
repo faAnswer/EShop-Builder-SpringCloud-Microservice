3b8d7422a8e964259f44e2977cf55eb0b8c8a1d7
ECSCloud <tecky23project@gmail.com>
2022-12-12 23:39:39 +0800

    20221212

--- product-service/src/main/java/org/tecky/productservice/controller/TestController.java
+++ product-service/src/main/java/org/tecky/productservice/controller/TestController.java
@@ -2,6 +2,7 @@ package org.tecky.productservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.tecky.productservice.entities.TypeEntity;
 import org.tecky.productservice.mapper.CategoryDetailEntityRepository;
@@ -13,17 +14,12 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class TestController {
 
-    @Autowired
-    TypeEntityRepository typeEntityRepository;
+    @GetMapping("/v1/test")
+    public String getTest()  {
 
-    @Autowired
-    CategoryChecker categoryChecker;
-    @GetMapping("/test")
-    public String getTest() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-
-        List<TypeEntity> categoryDTOList = typeEntityRepository.findAllValidType();
 
         return "test";
 
