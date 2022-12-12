18e3e2dac55c23d3b77aef96918b7046b2232962
faAnswer <oscarogc@gmail.com>
2022-12-13 03:30:42 +0800

    20221213

--- product-service/src/main/java/org/tecky/productservice/mapper/CategoryDetailEntityRepository.java
+++ product-service/src/main/java/org/tecky/productservice/mapper/CategoryDetailEntityRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface CategoryDetailEntityRepository extends JpaRepository<CategoryDetailEntity, String> {
 
 
-    @Query(nativeQuery = true, value = "SELECT tcd.category_ID, tcd.category_name FROM t_group_detail AS tgd INNER JOIN t_type AS tt ON tt.type_ID = tgd.type_ID INNER JOIN t_category_detail AS tcd ON tcd.category_ID = tt.category_ID WHERE tgd.client_ID = ?1")
+    @Query(nativeQuery = true, value = "SELECT DISTINCT tcd.category_ID, tcd.category_name FROM t_group_detail AS tgd INNER JOIN t_type AS tt ON tt.type_ID = tgd.type_ID INNER JOIN t_category_detail AS tcd ON tcd.category_ID = tt.category_ID WHERE tgd.client_ID = ?1")
     public List<CategoryDetailEntity> findAllCategoryByClientId(String clientId);
 
     @Query(value = "SELECT u FROM CategoryDetailEntity u")
