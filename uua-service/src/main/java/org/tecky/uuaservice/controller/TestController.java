3dad54f1cac45bad7fd272c2ca09becbb68233c0
ECSCloud <tecky23project@gmail.com>
2022-12-15 00:45:34 +0800

    20221215

--- uua-service/src/main/java/org/tecky/uuaservice/controller/TestController.java
+++ uua-service/src/main/java/org/tecky/uuaservice/controller/TestController.java
@@ -1,13 +1,26 @@
 package org.tecky.uuaservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.tecky.uuaservice.entities.UserSecClientEntity;
+import org.tecky.uuaservice.mapper.UserSecClientEntityRepository;
 
 @RestController
 public class TestController {
 
+    @Autowired
+    UserSecClientEntityRepository userSecClientEntityRepository;
     @GetMapping("/test")
     public String getTest(){
+
+        UserSecClientEntity userSecClientEntity = new UserSecClientEntity();
+
+        userSecClientEntity.setClientName("TestABC");
+        userSecClientEntity.setUsername("abcdefg");
+
+
+
 
         return "test";
     }
