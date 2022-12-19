2c7b14d324e2fbaa8685dd264816b4f3d1050091
faAnswer <oscarogc@gmail.com>
2022-12-20 05:02:09 +0800

    20221220

---
+++ warehouse-service/src/main/java/org/tecky/warehouseservice/mapper/InDetailM2SShipmentEntityRepository.java
@@ -0,0 +1,7 @@
+package org.tecky.warehouseservice.mapper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.tecky.warehouseservice.entities.InDetailM2SShipmentEntity;
+
+public interface InDetailM2SShipmentEntityRepository extends JpaRepository<InDetailM2SShipmentEntity, String> {
+}
---
+++ warehouse-service/src/main/java/org/tecky/warehouseservice/mapper/ShipmentS2MInDetailEntityRepository.java
@@ -0,0 +1,7 @@
+package org.tecky.warehouseservice.mapper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.tecky.warehouseservice.entities.ShipmentS2MInDetailEntity;
+
+public interface ShipmentS2MInDetailEntityRepository extends JpaRepository<ShipmentS2MInDetailEntity, String> {
+}
